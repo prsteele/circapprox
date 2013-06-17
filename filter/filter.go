0d6db38b1edf4a6b85339e26a65b21d74396990a
Patrick Steele <steele.pat@gmail.com>
2013-06-16 23:09:53 -0400

    Added a more robust set of command line options; the number, size, and opacity of approximations can now be set, along with the random number seed.

--- filter/filter.go
+++ filter/filter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/prsteele/circapprox"
@@ -12,7 +13,6 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
-	"time"
 )
 
 // Returns a function that will print a help string
@@ -29,80 +29,45 @@ func printHelp(flags *flag.FlagSet) func() {
 	}
 }
 
-func main() {
-	t1 := time.Now()
-
-	//
-	// Set up command-line flags
-	//
-	flags := flag.NewFlagSet("default", flag.ContinueOnError)
-	flags.Usage = printHelp(flags)
-
-	// Input and output files
-	in := flags.String("in", "", "The input file")
-	out := flags.String("out", "", "The input file")
-
-	// Parse the command line flags
-	if err := flags.Parse(os.Args[1:]); err != nil && err != flag.ErrHelp {
-		os.Exit(1)
-	} else if err == flag.ErrHelp {
-		os.Exit(0)
-	}
-
-	// Read in the source image
+// Attempt to decode the input as an Image. Returns a pointer to the
+// decoded image, a string representing the encoding used, and an
+// error, if any.
+func readInput(in string) (image.Image, string, error) {
 	var src image.Image
 	var src_fmt string
-	if *in == "" {
+	if in == "" {
 		// Read from standard input
 		var err error
 		if src, src_fmt, err = image.Decode(os.Stdin); err != nil {
-			panic(err)
+			return src, "", err
 		}
 	} else {
 		// Try to open the file
-		if f, err := os.Open(*in); err == nil {
-			var err2 error
-			if src, src_fmt, err2 = image.Decode(f); err2 != nil {
-				panic(err2)
-			}
+		if f, err := os.Open(in); err != nil {
+			return src, "", err
 		} else {
-			panic(err)
+			var dec_err error
+			if src, src_fmt, dec_err = image.Decode(f); dec_err != nil {
+				return src, "", dec_err
+			}
 		}
 	}
 
-	// Create the destination image
-	dst := image.NewRGBA64(src.Bounds())
+	return src, src_fmt, nil
+}
 
-	// Approximate the image
-	const (
-		N      = 3000
-		startR = 200
-		endR   = 10
-		R      = 60
-		r      = 20
-		rr     = 10
-		seed   = 0
-		alpha  = .75
-	)
-
-	rnd := rand.New(rand.NewSource(seed))
-
-	// Apply the filters
-	circles_large := circapprox.UniformCircles(src, 2*N, R, rnd)
-	circles_small := circapprox.UniformCircles(src, N, r, rnd)
-	circles_tiny := circapprox.UniformCircles(src, N/2, rr, rnd)
-	circapprox.Approximate(src, dst, alpha, circles_large)
-	circapprox.Approximate(src, dst, alpha, circles_small)
-	circapprox.Approximate(src, dst, alpha, circles_tiny)
-
-	// Get the output write
+// Attempt to write the Image `dst' to the file described by `out'. If
+// `out' is the empty string, we write to standard output, guessing at
+// the encoding using the input image format described by `src_fmt'.
+func writeOutput(dst image.Image, out, src_fmt string) error {
+	// Get the output writer
 	var writer io.Writer
-	if *out == "" {
+	if out == "" {
 		writer = os.Stdout
 	} else {
 		var err error
-		if writer, err = os.Create(*out); err != nil {
-			panic(err)
+		if writer, err = os.Create(out); err != nil {
+			return err
 		}
 	}
 
@@ -111,7 +76,7 @@ func main() {
 	// standard output, use the input encoding; if we can't output in
 	// that format, just use PNG.
 	var dst_fmt string
-	if *out == "" {
+	if out == "" {
 		if src_fmt == ".png" {
 			dst_fmt = src_fmt
 		} else if src_fmt == ".jpeg" || src_fmt == ".jpg" {
@@ -120,7 +85,7 @@ func main() {
 			dst_fmt = ".png"
 		}
 	} else {
-		ext := filepath.Ext(*out)
+		ext := filepath.Ext(out)
 		if ext == ".png" {
 			dst_fmt = ".png"
 		} else if ext == ".jpeg" || ext == ".jpg" {
@@ -128,7 +93,7 @@ func main() {
 		} else {
 			msg := fmt.Sprintf("Unknown output format '%s'; please use png or jpeg",
 				ext)
-			panic(msg)
+			return errors.New(msg)
 		}
 	}
 
@@ -140,10 +105,81 @@ func main() {
 	} else {
 		msg := fmt.Sprintf("Unknown output format '%s'; please use png or jpeg",
 			dst_fmt)
-		panic(msg)
+		return errors.New(msg)
 	}
 
-	dt := time.Since(t1)
+	return nil
+}
 
-	fmt.Println(float64(dt) / float64(time.Second))
+func validate(n int, r, a float64) error {
+	if n < 0 {
+		msg := fmt.Sprintf("Please specify a nonnegative number of "+
+			"approximation points, not %i", n)
+		return errors.New(msg)
+	}
+
+	if r < 0 {
+		msg := fmt.Sprintf("Please specify a nonnegative radius of "+
+			"approximation points, not %f", r)
+		return errors.New(msg)
+	}
+
+	if a < 0 || a > 1 {
+		msg := fmt.Sprintf("Please specify an alpha value between zero and one "+
+			", not %i", a)
+		return errors.New(msg)
+	}
+
+	return nil
+}
+
+func main() {
+	// Set up command-line flags
+	flags := flag.NewFlagSet("default", flag.ContinueOnError)
+	flags.Usage = printHelp(flags)
+
+	// Input and output file flags
+	in := flags.String("in", "", "The input file")
+	out := flags.String("out", "", "The input file")
+
+	// Approximation flags
+	var n int
+	var r float64
+	var a float64
+	var s int64
+	flags.IntVar(&n, "n", 100, "The number of points to approximate")
+	flags.Float64Var(&r, "r", 10, "The radius of approximation points")
+	flags.Float64Var(&a, "a", .75, "The alpha-value of approximated points")
+	flags.Int64Var(&s, "s", 0, "The random number seed")
+
+	// Parse the command line flags
+	if err := flags.Parse(os.Args[1:]); err != nil && err != flag.ErrHelp {
+		os.Exit(1)
+	} else if err == flag.ErrHelp {
+		os.Exit(0)
+	}
+
+	// Validate the inputs
+	if err := validate(n, r, a); err != nil {
+		panic(err)
+	}
+
+	// Read in the source image
+	src, src_fmt, src_err := readInput(*in)
+	if src_err != nil {
+		panic(src_err)
+	}
+
+	// The random number stream to use
+	rnd := rand.New(rand.NewSource(s))
+
+	// Create the destination image and approximate the source image
+	dst := image.NewRGBA64(src.Bounds())
+	circles := circapprox.UniformCircles(src, n, r, rnd)
+	circapprox.Approximate(src, dst, a, circles)
+
+	// Write the output
+	if dst_err := writeOutput(dst, *out, src_fmt); dst_err != nil {
+		panic(dst_err)
+	}
 }
